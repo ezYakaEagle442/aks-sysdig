3fb85d30d5a7452a9b26e0e67eedbb1782405178
ezYakaEagle442 <stepin.devops@outlook.com>
2023-07-22 14:20:21 +0200

    commented 02_hub

--- deploy.ps1
+++ deploy.ps1
@@ -3,22 +3,24 @@ rm terraform.tfstate
 rm terraform.tfstate.backup
 
 terraform init -upgrade
+terraform refresh
 terraform plan -out tfplan
 terraform apply tfplan
 
-cd ..\02_hub
-rm terraform.tfstate
-rm terraform.tfstate.backup
+#cd ..\02_hub
+#rm terraform.tfstate
+#rm terraform.tfstate.backup
 
-terraform init -upgrade
-terraform plan -out tfplan
-terraform apply tfplan
+#terraform init -upgrade
+#terraform plan -out tfplan
+#terraform apply tfplan
 
 cd ..\03_spoke_aks_infra
 rm terraform.tfstate
 rm terraform.tfstate.backup
 
 terraform init -upgrade
+terraform refresh
 terraform plan -out tfplan
 terraform apply tfplan
 
@@ -27,13 +29,15 @@ rm terraform.tfstate
 rm terraform.tfstate.backup
 
 terraform init -upgrade
+terraform refresh
 terraform plan -out tfplan
 terraform apply tfplan
 
-cd ..\05_monitoring
-rm terraform.tfstate
-rm terraform.tfstate.backup
+#cd ..\05_monitoring
+#rm terraform.tfstate
+#rm terraform.tfstate.backup
 
-terraform init -upgrade
-terraform plan -out tfplan
-terraform apply tfplan
+#terraform init -upgrade
+#terraform refresh
+#terraform plan -out tfplan
+#terraform apply tfplan
