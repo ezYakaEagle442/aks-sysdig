7be596a89d4f90ab1bcf923f6be29e90de466b13
ezYakaEagle442 <stepin.devops@outlook.com>
2023-07-23 12:59:44 +0200

    Set default system Pool VM Size to Standard_D8s_v5

--- 04_spoke_aks_cluster/aks.tf
+++ 04_spoke_aks_cluster/aks.tf
@@ -88,7 +88,7 @@ resource "azurerm_kubernetes_cluster" "aks" {
     max_count                    = 3
     max_pods                     = 110
     # https://learn.microsoft.com/en-us/azure/virtual-machines/dv5-dsv5-series
-    vm_size                      = "Standard_D4s_v5" # "Standard_D2s_v5"  "Standard_D2pds_v5" # "Standard_D2ds_v5" # "standard_d2pds_v5"
+    vm_size                      = "Standard_D8s_v5" # "Standard_D4s_v5" "Standard_D2s_v5"  "Standard_D2pds_v5" # "Standard_D2ds_v5" # "standard_d2pds_v5"
     os_disk_size_gb              = var.aks_agent_os_disk_size
     os_disk_type                 = "Managed" # "Ephemeral" # 
     ultra_ssd_enabled            = false
