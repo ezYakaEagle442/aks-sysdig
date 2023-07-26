bdb73c4671da3644dc44463000500fb6d4ea06c9
ezYakaEagle442 <stepin.devops@outlook.com>
2023-07-26 15:33:18 +0200

    add traces

--- deploy.ps1
+++ deploy.ps1
@@ -2,16 +2,20 @@ cd .\01_management
 rm terraform.tfstate
 rm terraform.tfstate.backup
 
-terraform init -upgrade
-terraform refresh
+terraform init # -upgrade
+# terraform refresh
 terraform plan -out tfplan
 terraform apply tfplan
+
+echo "###################################"
+echo "### 01_management completed       #"
+echo "###################################"
 
 #cd ..\02_hub
 #rm terraform.tfstate
 #rm terraform.tfstate.backup
 
-#terraform init -upgrade
+#terraform init # -upgrade
 #terraform plan -out tfplan
 #terraform apply tfplan
 
@@ -19,25 +23,33 @@ cd ..\03_spoke_aks_infra
 rm terraform.tfstate
 rm terraform.tfstate.backup
 
-terraform init -upgrade
-terraform refresh
+terraform init # -upgrade
+# terraform refresh
 terraform plan -out tfplan
 terraform apply tfplan
+
+echo "###################################"
+echo "### 03_spoke_aks_infra completed  #"
+echo "###################################"
 
 cd ..\04_spoke_aks_cluster
 rm terraform.tfstate
 rm terraform.tfstate.backup
 
-terraform init -upgrade
-terraform refresh
+terraform init # -upgrade
+# terraform refresh
 terraform plan -out tfplan
 terraform apply tfplan
+
+echo "###################################"
+echo "### 04_spoke_aks_cluster completed#"
+echo "###################################"
 
 #cd ..\05_monitoring
 #rm terraform.tfstate
 #rm terraform.tfstate.backup
 
-#terraform init -upgrade
-#terraform refresh
+#terraform init # -upgrade
+## terraform refresh
 #terraform plan -out tfplan
 #terraform apply tfplan
