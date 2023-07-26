5cbe7d1b6a45105271f1f6d53b7f528ac6f707e9
ezYakaEagle442 <stepin.devops@outlook.com>
2023-07-26 15:33:02 +0200

    connect Azure accounts to sysdig

---
+++ 042_sysdig/main.tf
@@ -0,0 +1,36 @@
+# https://eu1.app.sysdig.com/secure/#/onboarding/user-profile/cloud/2
+
+# Prerequisites
+# Contributor Azure role assignment
+#Terraform (version 1.3.1+) installed on the machine from which you will deploy the installation code
+# Or connect an entire Tenant instead.
+
+#Deploy with Terraform
+#This Terraform deployment will create a lighthouse definition  and the resources  required to process cloud logs.
+#Subscription ID
+#Copy the following and save it to a file named “main.tf”
+
+terraform {
+  required_providers {
+    sysdig = {
+      source = "sysdiglabs/sysdig"
+    }
+  }
+}
+
+provider "sysdig" {
+  sysdig_secure_url       = "https://eu1.app.sysdig.com"
+  sysdig_secure_api_token = "xxx"
+}
+
+provider "azurerm" {
+  features { }
+  subscription_id = ""
+}
+
+module "single-subscription" {
+  source = "sysdiglabs/secure-for-cloud/azurerm//examples/single-subscription"
+}
+
+# terraform init && terraform apply
+
